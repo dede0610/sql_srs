fdb6cce43a59d81cea4eddb4f13366103bac9b71
Dorian Dessa <Dorian.dessa@fr.ey.com>
2023-11-16 18:47:43 +0100

    commit premiere version toute simple de l'app

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print('test')
+import streamlit as st
+import pandas as pd
+import duckdb as db
+
+data = {"a": [1, 2, 3], "b": [4, 5, 6]}
+df = pd.DataFrame(data)
+st.dataframe(df)
+
+q = st.text_area(label="Rentrer la query que vous souhaitez exécuter sachant que la table master ci-dessus est intitulée 'df':"
+                 ,value="SELECT * FROM df"
+                 )
+st.write(f"Voici la requête renseignée : {q}")
+
+df_query = db.sql(str(q)).df()
+st.dataframe(df_query)
