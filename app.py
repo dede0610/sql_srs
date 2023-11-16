ae76a7c72e6bae7937527e814ef2ae8791e173a9
Dorian Dessa <Dorian.dessa@fr.ey.com>
2023-11-16 18:50:00 +0100

    correction wording

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
 st.dataframe(df)
 
-q = st.text_area(label="Rentrer la query que vous souhaitez exécuter sachant que la table master ci-dessus est intitulée 'df':"
-                 ,value="SELECT * FROM df"
+q = st.text_area(label="Renseigner la requête que vous souhaitez exécuter sachant que la table master ci-dessus est intitulée 'df':",
+                 value="SELECT * FROM df"
                  )
-st.write(f"Voici la requête renseignée : {q}")
+st.write(f"Voici le resultat de la requête suivante : {q}")
 
 df_query = db.sql(str(q)).df()
 st.dataframe(df_query)
